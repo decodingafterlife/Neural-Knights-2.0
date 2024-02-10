ed40eeceba6d5ec47e22209255876e6dc475eea1
ameyjoshi3068 <ameyjoshi3068@outlook.com>
2024-02-11 00:39:11 +0530

    integrating docker

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const express = require("express");
 const app = express();
 const { PythonShell } = require("python-shell");
 
-app.get("/", (req, res, next) => {
+app.get("/api", (req, res) => {
+  var flag = 0;
+  var target = req.query.target;
+  if (target) flag += 1;
+  var video = req.query.video;
+  if (video) flag += 1;
+
   let options = {
     mode: "text",
     pythonOptions: ["-u"],
-    scriptPath: "path/to/my/scripts",
-    args: [`${req.body}`],
+    scriptPath: "./interface",
+    args: [flag, target, video],
   };
+
+  try {
+    PythonShell.run("main.py", options, function (err, result) {
+      if (err) throw err;
+      // result is an array consisting of messages collected
+      //during execution of script.
+      console.log("result: ", result.toString());
+      res.status(200).json("./output.json");
+    });
+  } catch {
+    console.log("Error");
+  }
 });
+
+const port = 8000;
+app.listen(port, () => console.log(`Server connected to ${port}`));
---
+++ interface/main.py
@@ -0,0 +1,9 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import youtube as yt
+from target_based_analysis_page import target
+from video_based_analysis_page import video
+from aboutus import about_us
+from PIL import Image 
+print("Hello World")
